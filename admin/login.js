5fb18ced992e734e6d7a89cd66ba288ca6d15ba0
Mudambaelvis <you@example.com>
2026-04-10 16:47:36 +0300

    api

--- admin/login.js
+++ admin/login.js
@@ -1,6 +1,9 @@
-const API_BASE = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
-    ? "http://localhost:5000/api"
-    : "https://alexia-tours-backend-production.up.railway.app/api";
+// const API_BASE = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
+//     ? "http://localhost:5000/api"
+//     : "https://alexia-tours-backend-production.up.railway.app/api";
+
+
+const API_BASE = "https://alexia-tours-backend-production.up.railway.app/api";
 
 
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
